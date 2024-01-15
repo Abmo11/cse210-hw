d30891e8eeff05e4ed34dec544a8d979727a6db3
Abmo11 <mor18053@byui.edu>
2024-01-15 09:50:31 -0600

    Core requirements 1-2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Asks for a magic number and stores it in variable
+        Console.Write("What is the magic number? ");
+        string magicInput = Console.ReadLine();
+        // Parses to integer
+        int magicNumber = int.Parse(magicInput);
+
+        // Variable for use in while loop
+        int guess;
+
+        // Compares input to guess using do while loop
+        do
+        {
+            // Asks for a guess and stores it in variable
+            Console.Write("What is your guess? ");
+            string guessInput = Console.ReadLine();
+            // Parses to integer
+            guess = int.Parse(guessInput);
+            
+            // Compares guess to input
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != magicNumber);
     }
 }
